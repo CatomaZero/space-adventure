8c8648f4106663eba07e1445b7ecfb59afdb4e59
SaraLuciaa <saludipu71@gmail.com>
2023-11-25 11:31:22 -0500

    How To Play - Screen

--- src/main/java/ui/HowToPlayController.java
+++ src/main/java/ui/HowToPlayController.java
@@ -1,3 +1,47 @@
 package ui;
-public class HowToPlayController {
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HowToPlayController implements Initializable {
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private Button home;
+
+    @FXML
+    private Button play;
+
+    @FXML
+    private void onHome() {
+        HelloApplication.hideWindow((Stage) play.getScene().getWindow());
+        HelloApplication.showWindow("hello-view", null);
+    }
+
+    @FXML
+    private void onPlay() {
+        HelloApplication.hideWindow((Stage) play.getScene().getWindow());
+        HelloApplication.showWindow("play", null);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.web("#001020"));
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        gc.drawImage(new Image(getClass().getResource(("/assets/background/howToPlay.png")).toExternalForm()), 0, 0, 1000, 700);
+
+    }
 }
